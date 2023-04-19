eb370c1fa2a1704d3140bee479355c704c089c71
Fulkurun <thefulkurun@gmail.com>
2023-04-19 12:11:43 -0300

    Revert "Delete PrePr2Ej01.py"
    This reverts commit deb83d10aa61880432c31c4c8199c2b0af707f1a.

---
+++ TrabajoN2/PrePr2Ej01.py
@@ -0,0 +1,16 @@
+#Crear un programa que permita ingresar una lista de numeros al usuario y muestre por pantalla el maximo entre ambos numeros.
+
+#Nota : Hacerlo con la función max(a,b) y luego con una comparación
+
+#INICIO
+numeros = input("Ingresa una lista de números separados por comas: ")
+numeros = [int(x.strip()) for x in numeros.split(",")]
+maximo = max(numeros)
+print("El máximo es:", maximo)
+maximo = numeros[0]
+for num in numeros:
+    if num > maximo:
+        maximo = num
+print("El máximo es:", maximo)
+#FIN
+
